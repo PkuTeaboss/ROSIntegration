1a9d1a15c7a825df9b2c2306cc203bfe51f91c72
PkuTeaboss <yixinyang_pku@outlook.com>
2019-01-29 12:15:20 -0800

    clean code

--- Source/ROSIntegration/Public/sensor_msgs/CompressedImage.h
+++ Source/ROSIntegration/Public/sensor_msgs/CompressedImage.h
@@ -4,21 +4,29 @@
 #include "std_msgs/Header.h"
 
 namespace ROSMessages {
-namespace sensor_msgs {
-class CompressedImage : public FROSBaseMsg {
- public:
-  CompressedImage() {
-    _MessageType = "sensor_msgs/CompressedImage";
-  }
+    namespace sensor_msgs {
+        class CompressedImage : public FROSBaseMsg {
+        public:
+            CompressedImage() {
+            _MessageType = "sensor_msgs/CompressedImage";
+            }
 
-  ROSMessages::std_msgs::Header header;
+           /** Header timestamp should be acquisition time of image
+            *  Header frame_id should be optical frame of camera
+            *  origin of frame should be optical center of camera
+            *  +x should point to the right in the image
+            *  +y should point down in the image
+            *  +z should point into to plane of the image
+            */
+            ROSMessages::std_msgs::Header header;
 
-  FString format;
+            FString format;         // Specifies the format of the data
+                                    //   Acceptable values: jpeg, png
 
-  const uint8* data;
+            const uint8* data;      // Compressed image buffer
 
-  int data_size;
+            int data_size;          // image buffer size
 
-};
-}
+        };
+    }
 }
